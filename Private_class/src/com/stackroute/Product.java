9247ecb2814e8d969ecba837a9611eb91968195a
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 12:58:44 +0000

    private class

--- Private_class/src/com/stackroute/Product.java
+++ Private_class/src/com/stackroute/Product.java
@@ -1,13 +1,15 @@
 package com.stackroute;
 
 public class Product {
+    // Creating private instance variable.
     private String productCode;
     private String productName;
     private double productPrice;
     private char categoryCode;
-
+// Creating private static integer with value initialized 100
     private static int num= 100;
 
+    // Creating getter and setter
     public String getProductCode() {
         return productCode;
     }
@@ -46,5 +48,46 @@ public class Product {
 
     public static void setNum(int num) {
         Product.num = num;
+    }
+
+
+    // Method to generate the Product code as String which will merge Catogery Code and Incremented product counter
+    private String generateProductCode()
+    {
+        num++;
+        return (Character.toString(categoryCode)+Integer.toString(num));
+    }
+
+    // Initializing constructor of 3 parameter
+    public Product(String productName, double productPrice, char categoryCode) {
+        // Initialize variables
+        this.productCode = "P101";
+        this.productName = "Laptop";
+        this.productPrice = 45000;
+        this.categoryCode = 'F';
+
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.categoryCode = categoryCode;
+        this.productCode = generateProductCode();
+
+    }
+    // Initializing constructor of 2 parameter
+    public Product(String productName, double productPrice) {
+        //Initializing variables
+        this.productCode = "P101";
+        this.productName = "Laptop";
+        this.productPrice = 45000;
+        this.categoryCode ='E';
+
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productCode = generateProductCode();
+    }
+
+    // method to format output in string format of String return type
+    public String getProductDetails(){
+        String con = ("Code-"+productCode+", Name-"+productName+",price-"+Double.toString(productPrice)+", Category-"+Character.toString(categoryCode));
+        return con;
     }
 }
