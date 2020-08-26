6867e244ad543ff1d92e6148c3292ab320d1f149
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 14:02:52 +0000

    Adding program to show constructor

---
+++ Class_constructor/src/com/stackroute/SimpleObject.java
@@ -0,0 +1,8 @@
+package com.stackroute;
+// Creating class SimpleObject.
+public class SimpleObject {
+    //Creating Constructor which displaying a message.
+    public SimpleObject() {
+        System.out.println("Hi this is a sample program to demonstrate Constructor!");
+    }
+}
---
+++ Class_constructor/src/com/stackroute/TestSimpleObject.java
@@ -0,0 +1,9 @@
+package com.stackroute;
+// Main Class
+public class TestSimpleObject {
+
+    public static void main(String[] args) {
+        // Creating object for the class SimpleObject.
+	    SimpleObject demo = new SimpleObject();
+    }
+}
