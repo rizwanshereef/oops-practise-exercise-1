5e6816709d4f16e08d63863daf9f582acead8fa6
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-27 11:22:39 +0000

    update Product- naming convention

--- createclass_getter_setter/src/com/stackroute/TestProduct.java
+++ createclass_getter_setter/src/com/stackroute/TestProduct.java
@@ -4,15 +4,15 @@ public class TestProduct {
 
     public static void main(String[] args) {
 
-        Product p1 = new Product();
+        Product product1 = new Product();
         //assigning variables using setter
-        p1.setProductCode("P101");
-        p1.setProductName("Laptop");
-        p1.setProductPrice(45000.00);
+        product1.setProductCode("P101");
+        product1.setProductName("Laptop");
+        product1.setProductPrice(45000.00);
         //Display values using getter
-        System.out.println("Product Code:"+p1.getProductCode());
-        System.out.println("Product Name:"+p1.getProductName());
-        System.out.println("Product Price:"+p1.getProductPrice());
+        System.out.println("Product Code:"+product1.getProductCode());
+        System.out.println("Product Name:"+product1.getProductName());
+        System.out.println("Product Price:"+product1.getProductPrice());
 
 
     }
